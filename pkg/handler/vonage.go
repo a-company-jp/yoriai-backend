15643b96a83f971e6a636cfe35abf35a69c04239
Najah7 <najahmatsuo7@gmail.com>
2024-10-12 05:21:38 +0900

    🏷️ [Add] `VonageWebhookRequest`

--- pkg/handler/vonage.go
+++ pkg/handler/vonage.go
@@ -2,6 +2,16 @@ package handler
 
 import "github.com/gin-gonic/gin"
 
+type VonageWebhookRequest struct {
+	AgentID string `json:"agent_id"`
+	SessionID string `json:"session_id"`
+	ConversationID string `json:"conversation_id"`
+	Feeling string `json:"feeling"`
+	PhoneNumber string `json:"phone_number"`
+	Message string `json:"message"`
+	TodayActivity string `json:"today_activity"`
+}
+
 type VonageWebhook struct {
 }
 
@@ -10,7 +20,13 @@ func NewVonageWebhook() *VonageWebhook {
 }
 
 func (v *VonageWebhook) Handle(c *gin.Context) {
+	var req VonageWebhookRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	// TODO: forward the request to LINE
 	c.JSON(200, gin.H{
-		"message": "Hello World",
+		"message": "success",
 	})
 }
