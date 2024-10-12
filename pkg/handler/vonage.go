42728e6f55466b4622167eb0574fc22e2563219a
Shion Ichikawa <shion1305@gmail.com>
2024-10-12 11:39:07 +0900

    ✨ NotifyText generation

--- pkg/handler/vonage.go
+++ pkg/handler/vonage.go
@@ -1,8 +1,12 @@
 package handler
 
 import (
+	"cloud.google.com/go/firestore"
+	"fmt"
+	"github.com/a-company/yoriai-backend/pkg/model"
 	"github.com/a-company/yoriai-backend/pkg/service/line"
 	"github.com/gin-gonic/gin"
+	"log/slog"
 )
 
 type VonageWebhookRequest struct {
@@ -17,11 +21,16 @@ type VonageWebhookRequest struct {
 
 type VonageWebhook struct {
 	line *line.LINEBotService
+	fs   *firestore.Client
 }
 
-func NewVonageWebhook(svc line.LINEBotService) *VonageWebhook {
+func NewVonageWebhook(
+	svc *line.LINEBotService,
+	fs *firestore.Client,
+) *VonageWebhook {
 	return &VonageWebhook{
-		line: &svc,
+		line: svc,
+		fs:   fs,
 	}
 }
 
@@ -31,9 +40,32 @@ func (v *VonageWebhook) Handle(c *gin.Context) {
 		c.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
-	// TODO: forward the request to LINE
-	v.line.ReplyTextMessage(req.ConversationID, req.Message)
 
+	userdata := model.User{}
+	doc, err := v.fs.Collection("users").
+		Where("phone_number", "==", req.PhoneNumber).
+		Documents(c).Next()
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+	doc.DataTo(&userdata)
+
+	//気分: Feeling
+	//今日の通話のサマリー: TodayActivity
+	//伝言: Message
+
+	notifyText := "本日の通話が終了しました\n\n"
+	notifyText += fmt.Sprintf("気分: %s\n", req.Feeling)
+	notifyText += fmt.Sprintf("今日の通話のサマリー: %s\n", req.TodayActivity)
+	notifyText += fmt.Sprintf("伝言: %s\n", req.Message)
+
+	slog.Info("notifyText", slog.String("notifyText", notifyText))
+	if err := v.line.PushTextMessage(userdata.LINEID, notifyText); err != nil {
+		slog.Error("failed to push message", err)
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
 	c.JSON(200, gin.H{
 		"message": "success",
 	})
